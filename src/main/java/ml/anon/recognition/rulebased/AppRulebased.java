43098c67cebf2419f0694bd7023515e60209c0c1
Mirco <ablx@posteo.de>
2017-05-13 17:44:09 +0200

    Spring Boot Boilerplate

---
+++ src/main/java/ml/anon/recognition/rulebased/AppRulebased.java
@@ -0,0 +1,13 @@
+package ml.anon.recognition.rulebased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRulebased {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRulebased.class, args);
+
+    }
+}
