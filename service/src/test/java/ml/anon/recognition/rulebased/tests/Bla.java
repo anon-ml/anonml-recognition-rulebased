643fdebacd73b0e517afbf07021bb4a37abfecaf
Mirco <ablx@posteo.de>
2017-08-16 14:49:56 +0200

    It was the best of times, it was the worst of times

---
+++ service/src/test/java/ml/anon/recognition/rulebased/tests/Bla.java
@@ -0,0 +1,17 @@
+package ml.anon.recognition.rulebased.tests;
+
+import ml.anon.recognition.rulebased.api.model.RegExpRule;
+import ml.anon.recognition.rulebased.api.model.Rule;
+import org.junit.Test;
+
+/**
+ * Created by mirco on 16.08.17.
+ */
+public class Bla {
+
+  @Test
+  public void test() {
+    Rule rule = new RegExpRule();
+  }
+
+}
