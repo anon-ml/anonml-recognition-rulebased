67e3a0e656b18ef2e4e9ccb856bdca13e22bb727
Mirco <ablx@posteo.de>
2017-09-10 14:22:14 +0200

    Admin UI Dashboard

--- api/src/main/java/ml/anon/recognition/rulebased/api/model/Rule.java
+++ api/src/main/java/ml/anon/recognition/rulebased/api/model/Rule.java
@@ -6,6 +6,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import javax.validation.constraints.NotNull;
+
 import lombok.Builder;
 import lombok.Data;
 import lombok.Singular;
@@ -15,6 +16,7 @@ import ml.anon.anonymization.model.Producer;
 import ml.anon.documentmanagement.model.Document;
 import ml.anon.anonymization.model.Replacement;
 import ml.anon.documentmanagement.resource.ReplacementResource;
+import ml.anon.model.BaseEntity;
 import ml.anon.recognition.rulebased.api.constraints.Constraint;
 import org.springframework.data.annotation.Id;
 
@@ -25,54 +27,52 @@ import org.springframework.data.annotation.Id;
 @Builder
 @Data
 @org.springframework.data.mongodb.core.mapping.Document(collection = "Rule")
-public class Rule implements Applicable {
+public class Rule extends BaseEntity implements Applicable {
 
-  @Id
-  private String id;
-  @NotNull
-  private String regExp;
+    @NotNull
+    private String regExp;
 
-  private boolean core;
-  private boolean active = true;
-  private boolean editable = false;
-  private double weight;
-  private String name;
+    private boolean core;
+    private boolean active = true;
+    private boolean editable = false;
+    private double weight;
+    private String name;
 
-  @NotNull
-  private Label label;
+    @NotNull
+    private Label label;
 
-  @Singular
-  private List<Constraint> constrains;
+    @Singular
+    private List<Constraint> constrains;
 
-  public Rule copy() {
-    return Rule.builder().active(active).regExp(regExp).core(false).active(active).weight(weight)
-        .editable(true).weight(weight).name(name + " (copy)").label(label).build();
-  }
-
-
-  @Override
-  public List<Anonymization> apply(Document doc, ReplacementResource repl) throws Exception {
-    Matcher matcher = Pattern.compile(regExp).matcher(doc.getFullText());
-    List<Anonymization> regExpResults = new ArrayList<>();
-
-    while (matcher.find()) {
-      regExpResults.add(Anonymization.builder()
-          .data(
-              repl.create(Replacement.builder().label(label).original(matcher.group(0)).build()))
-          .producer(Producer.RULE).build());
+    public Rule copy() {
+        return Rule.builder().active(active).regExp(regExp).core(false).active(active).weight(weight)
+                .editable(true).weight(weight).name(name + " (copy)").label(label).build();
     }
-    return applyConstrains(regExpResults);
-  }
 
-  protected List<Anonymization> applyConstrains(List<Anonymization> anonymizations) {
-    if (constrains != null) {
-      return anonymizations.stream()
-          .filter(s -> constrains.stream().allMatch(c -> c.test(s.getData().getOriginal())))
-          .collect(
-              Collectors.toList());
+
+    @Override
+    public List<Anonymization> apply(Document doc, ReplacementResource repl) throws Exception {
+        Matcher matcher = Pattern.compile(regExp).matcher(doc.getFullText());
+        List<Anonymization> regExpResults = new ArrayList<>();
+
+        while (matcher.find()) {
+            regExpResults.add(Anonymization.builder()
+                    .data(
+                            repl.create(Replacement.builder().label(label).original(matcher.group(0)).build()))
+                    .producer(Producer.RULE).build());
+        }
+        return applyConstrains(regExpResults);
     }
-    return anonymizations;
 
-  }
+    protected List<Anonymization> applyConstrains(List<Anonymization> anonymizations) {
+        if (constrains != null) {
+            return anonymizations.stream()
+                    .filter(s -> constrains.stream().allMatch(c -> c.test(s.getData().getOriginal())))
+                    .collect(
+                            Collectors.toList());
+        }
+        return anonymizations;
+
+    }
 
 }
--- api/src/main/java/ml/anon/recognition/rulebased/api/resource/RuleResource.java
+++ api/src/main/java/ml/anon/recognition/rulebased/api/resource/RuleResource.java
@@ -6,10 +6,7 @@ import javax.annotation.PostConstruct;
 
 import lombok.extern.slf4j.Slf4j;
 import ml.anon.recognition.rulebased.api.model.Rule;
-import ml.anon.resource.Create;
-import ml.anon.resource.Delete;
-import ml.anon.resource.Read;
-import ml.anon.resource.Update;
+import ml.anon.resource.*;
 
 import org.springframework.beans.factory.annotation.Value;
 
@@ -25,61 +22,62 @@ import org.springframework.web.client.RestTemplate;
  */
 @Slf4j
 @Component
-public class RuleResource implements Create<Rule>, Read<Rule>, Update<Rule>,
-    Delete {
+public class RuleResource implements Create<Rule>, Read<Rule>, ReadAll<Rule>, Update<Rule>,
+        Delete {
 
 
-  private RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate restTemplate = new RestTemplate();
 
-  @Value("${rulebased.service.url}")
-  private String rulebasedUrl;
-  private String baseUrl;
+    @Value("${rulebased.service.url}")
+    private String rulebasedUrl;
 
-
-  @PostConstruct
-  public void init() {
-    baseUrl = rulebasedUrl + "/api/rule";
-  }
+    private String baseUrl;
 
 
-  @Override
-  public Rule findById(String id) {
-    log.debug("Find Applicable with id {}", id);
-    ResponseEntity<Rule> response = restTemplate
-        .getForEntity(baseUrl + "/{id}", Rule.class, id);
-    return response.getBody();
-  }
+    @PostConstruct
+    public void init() {
+        baseUrl = rulebasedUrl + "/api/rule";
+    }
 
-  @Override
-  public List<Rule> findAll() {
-    log.debug("Find all rules");
-    ResponseEntity<List<Rule>> result = restTemplate
-        .exchange(baseUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<Rule>>() {
-        });
-    return result.getBody();
-  }
 
-  @Override
-  public Rule update(String id, Rule instance) {
-    log.debug("Update rule {}", instance);
-    ResponseEntity<Rule> exchange = restTemplate
-        .exchange(baseUrl + "/{id}", HttpMethod.PUT, new HttpEntity<>(instance), Rule.class,
-            id);
-    return exchange.getBody();
-  }
+    @Override
+    public Rule findById(String id) {
+        log.debug("Find Applicable with id {}", id);
+        ResponseEntity<Rule> response = restTemplate
+                .getForEntity(baseUrl + "/{id}", Rule.class, id);
+        return response.getBody();
+    }
 
-  @Override
-  public Rule create(Rule instance) {
-    log.debug("Create rule {}", instance);
-    ResponseEntity<Rule> exchange = restTemplate
-        .exchange(baseUrl + "", HttpMethod.POST, new HttpEntity<>(instance), Rule.class
-        );
-    return exchange.getBody();
-  }
+    @Override
+    public List<Rule> findAll() {
+        log.debug("Find all rules");
+        ResponseEntity<List<Rule>> result = restTemplate
+                .exchange(baseUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<Rule>>() {
+                });
+        return result.getBody();
+    }
 
-  @Override
-  public void delete(String id) {
-    log.debug("Delete rule with id {}", id);
-    restTemplate.delete(baseUrl + "/{id}", id);
-  }
+    @Override
+    public Rule update(String id, Rule instance) {
+        log.debug("Update rule {}", instance);
+        ResponseEntity<Rule> exchange = restTemplate
+                .exchange(baseUrl + "/{id}", HttpMethod.PUT, new HttpEntity<>(instance), Rule.class,
+                        id);
+        return exchange.getBody();
+    }
+
+    @Override
+    public Rule create(Rule instance) {
+        log.debug("Create rule {}", instance);
+        ResponseEntity<Rule> exchange = restTemplate
+                .exchange(baseUrl + "", HttpMethod.POST, new HttpEntity<>(instance), Rule.class
+                );
+        return exchange.getBody();
+    }
+
+    @Override
+    public void delete(String id) {
+        log.debug("Delete rule with id {}", id);
+        restTemplate.delete(baseUrl + "/{id}", id);
+    }
 }
